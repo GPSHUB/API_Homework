3bf3b5af0253afd509041a6ece4b8d56b0b1ee06
GiamSigaud <giam_sigaud@hotmail.com>
2020-08-16 09:58:47 -0500

    Committing Current Weather.py

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++ 06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "7d1f2c84620e7adb8d7b17ac24e9f135"
+weather_api_key = "6ef94db09ea107ccfbf35e1a2b3ab3b0"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDf8iZy7HN6HJwd9xkLAOHD9pcCdG8JoU4!"
