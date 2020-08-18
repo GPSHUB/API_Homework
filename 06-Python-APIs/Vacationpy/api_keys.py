c6c9f88b8877064118fe95017b3588391fe9df29
GiamSigaud <giam_sigaud@hotmail.com>
2020-08-18 15:54:37 -0500

    Final HW6 Commit

---
+++ 06-Python-APIs/Vacationpy/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "Code removed due to pushing to Public GIT HUB Repo - Giam"
+
+# Google API Key
+g_key = "Code removed due to pushing to Public GIT HUB Repo - Giam"
+
---
+++ 06-Python-APIs/Weatherpy/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "Key removed due to pushing to Public GIT HUB Repo - Giam"
+
+# Google API Key
+g_key = "Key removed due to pushing to Public GIT HUB Repo - Giam"
+
