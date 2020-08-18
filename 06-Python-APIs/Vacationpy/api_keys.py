7dea6fb58cb5eff7ed4750568d9eeb9bc5af7460
GiamSigaud <giam_sigaud@hotmail.com>
2020-08-18 18:31:21 -0500

    Final HW commit - pngs fixed

--- 06-Python-APIs/Vacationpy/api_keys.py
+++ 06-Python-APIs/Vacationpy/api_keys.py
@@ -1,7 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "Deleted for Public Repo Storage -Giam"
+weather_api_key = "Deleted Key for Public Repo storage -Giam"
 
 # Google API Key
-g_key = "Deleted for Public Repo Storage -Giam"
-
-
+g_key = "Deleted Key for Public Repo storage -Giam"
